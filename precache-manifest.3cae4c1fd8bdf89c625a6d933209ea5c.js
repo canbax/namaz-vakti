ed278ed12b509daa78fa663ccdb12292263df31d
canbax <yusufsaidcanbaz@gmail.com>
2022-09-24 19:39:07 +0300

    push new version using Vercel as API server

---
+++ precache-manifest.3cae4c1fd8bdf89c625a6d933209ea5c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5eb7237281a1e11f0f",
+    "url": "/namaz-vakti/css/app.383863b1.css"
+  },
+  {
+    "revision": "af5eb7237281a1e11f0f",
+    "url": "/namaz-vakti/css/app.383863b1.css.map"
+  },
+  {
+    "revision": "003847349420bfe66e0f",
+    "url": "/namaz-vakti/css/chunk-vendors.9d3d6684.css"
+  },
+  {
+    "revision": "003847349420bfe66e0f",
+    "url": "/namaz-vakti/css/chunk-vendors.9d3d6684.css.map"
+  },
+  {
+    "revision": "2b8c59bfc92f851c3002c17dc05bdb26",
+    "url": "/namaz-vakti/index.html"
+  },
+  {
+    "revision": "af5eb7237281a1e11f0f",
+    "url": "/namaz-vakti/js/app-legacy.8e26bd2b.js"
+  },
+  {
+    "revision": "af5eb7237281a1e11f0f",
+    "url": "/namaz-vakti/js/app-legacy.8e26bd2b.js.map"
+  },
+  {
+    "revision": "003847349420bfe66e0f",
+    "url": "/namaz-vakti/js/chunk-vendors-legacy.d4fcbcb7.js"
+  },
+  {
+    "revision": "003847349420bfe66e0f",
+    "url": "/namaz-vakti/js/chunk-vendors-legacy.d4fcbcb7.js.map"
+  },
+  {
+    "revision": "1739f89d60d3bc90bf1e774e7f705c08",
+    "url": "/namaz-vakti/manifest.json"
+  }
+]);
